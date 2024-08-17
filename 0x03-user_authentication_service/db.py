5447f18a38224e035284ef5a4cc2158dbef5eab2
amutsamoses <amutsamoses@gmail.com>
2024-08-17 01:33:02 -0700

    find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,6 +8,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -61,3 +63,36 @@ class DB:
             raise
 
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+         Finds a user based on keyword arguments.
+
+         Args:
+            **kwargs: Arbitrary keyword arguments used for filtering the query.
+
+        Return:
+            The first user that matches the filtering criteria.
+            or reises an exception
+
+        Raises:
+            NoResultFound: no results found matching filter criteria
+            InvalidRequestError: when wrong query arguments are passed
+
+            """
+
+        query = self._session.query(User)
+
+        'filter based on the provided arbitrary keyword argument'
+        for key, value in kwargs.items():
+            'check if the attribute exist in User model'
+            if not hasattr(User, key):
+                raise InvalidRequestError(f"Invalid Argument passed: {key}")
+            query = query.filter(getattr(User, key) == value)
+
+        user = query.one()
+
+        if user is None:
+            raise NoResultFount("No user Found")
+
+        return user
